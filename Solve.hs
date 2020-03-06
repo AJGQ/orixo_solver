3131dc37cc26e254ff3601aeb33820717c2ef952
artur <artur11jorge@gmail.com>
2020-03-06 00:33:20 +0000

    added some finders

--- Solve.hs
+++ Solve.hs
@@ -41,8 +41,7 @@ sameCellNumber (Orixo wm ic) =
 
 --------------------------------------------------
 linesFrom :: Orixo -> Cell -> M.Map Direction (S.Set Cell)
-linesFrom o@(Orixo wm ic) c =
-    M.fromList $ zip dirs $ zipWith (lineFrom o) (repeat c) dirs
+linesFrom o@(Orixo wm ic) c = M.fromList $ zip dirs $ map ((lineFrom o) c) dirs
   where
     dirs = [U, D, L, R]
 
@@ -75,15 +74,91 @@ dependencies o@(Orixo wm ic) =
 all_cells :: M.Map Cell (M.Map Direction (S.Set Cell)) -> S.Set Cell
 all_cells = S.unions . M.map (\m -> S.unions $ S.map (m M.!) $ M.keysSet m)
 
-single_dependent :: Orixo -> M.Map Cell (S.Set Cell)
+find_direction :: Cell -> S.Set Cell -> Maybe Direction
+find_direction (x, y) sc
+    | all (\(x', y') -> x == x' && y < y') $ S.toList sc = Just U
+    | all (\(x', y') -> x == x' && y > y') $ S.toList sc = Just D
+    | all (\(x', y') -> y == y' && x < x') $ S.toList sc = Just R
+    | all (\(x', y') -> y == y' && x > x') $ S.toList sc = Just L
+    | otherwise = Nothing
+
+single_dependent :: Orixo -> M.Map Cell (Direction, S.Set Cell)
 single_dependent o =
     let deps = dependencies o
         no_key k = M.delete k deps
-     in M.mapWithKey
-            (\k a -> all_cells (M.fromList [(k, a)]) S.\\ all_cells (no_key k))
+     in M.filter (not . null . snd) $
+        M.mapWithKey
+            (\k a ->
+                 (\sc ->
+                      let Just d = find_direction k sc
+                       in (d, sc)) $
+                 (all_cells (M.fromList [(k, a)]) S.\\ all_cells (no_key k)))
             deps
 
 --------------------------------------------------
+obligated_finder :: Orixo -> M.Map Cell (Maybe Direction)
+obligated_finder o@(Orixo _ ic) =
+    let deps = dependencies o
+        volume = (ic M.!)
+     in M.mapWithKey
+            (\c mds ->
+                 (\ks ->
+                      if S.size ks == 1
+                          then Just . head . S.toList $ ks
+                          else Nothing) .
+                 M.keysSet . M.filter ((== volume c) . S.size) $
+                 mds)
+            deps
+
+--------------------------------------------------
+discovered_finder :: Orixo -> M.Map Cell (Maybe Direction)
+discovered_finder o@(Orixo _ ic) =
+    let mcds = single_dependent o
+        volume = (ic M.!)
+     in M.mapWithKey
+            (\c (d, sc) ->
+                 if S.size sc == volume c
+                     then Just d
+                     else Nothing)
+            mcds
+
+--------------------------------------------------
+two_sided_impossibility_finder :: Orixo -> S.Set Cell
+two_sided_impossibility_finder o =
+    let deps = dependencies o
+        no_key k = M.delete k deps
+     in M.keysSet .
+        M.filter id .
+        M.mapWithKey
+            (\k a ->
+                 maybe True (const False) $
+                 find_direction k $
+                 (all_cells (M.fromList [(k, a)]) S.\\ all_cells (no_key k))) $
+        deps
+
+--------------------------------------------------
+no_options :: Orixo -> S.Set Cell
+no_options o@(Orixo _ ic) =
+    let deps = dependencies o
+        --undefined
+     in M.keysSet .
+        M.filter M.null .
+        M.mapWithKey (\c mdsc -> M.filter (\sc -> S.size sc >= ic M.! c) mdsc) $
+        deps
+
+--------------------------------------------------
+-- obligated :  when the only cells possible to slide are the same number as 
+--              the volume of the starter (DONE)
+-- 
+-- discovered : when the single dependents are the same number as the volume
+--              of the starter (DONE)
+--
+-- two_sided_impossibility :    when a cell as 2 single dependents or more in 
+--                              different directions (DONE)
+--
+-- no_options : it's impossible to slide the starter in any direction (DONE)
+-- 
+-- 
 solve :: Orixo -> Maybe Solution
 solve o@(Orixo wm ic)
     | all ($ o) filters = solveChunk o
